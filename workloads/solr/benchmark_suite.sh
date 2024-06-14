9087ec74b83bee161d672df51bc9104c7ec8b13c
Yannik Lubas <yannik.lubas@uni-wuerzburg.de>
2024-06-14 11:54:50 +0200

    FEAT: Automatic jar copy

--- workloads/solr/benchmark_suite.sh
+++ workloads/solr/benchmark_suite.sh
@@ -7,9 +7,20 @@ TIMEOUT_MS=3000
 WARMUP_DURATION_SEC=120
 WARMUP_RPS=25
 WARMUP_PAUSE_SEC=10
+LOAD_GENERATOR_LOC="$HOME/load_generator"
 
 # Download dataset if the web_search_data container does not exist
 ssh "$USER"@"$SERVER_IP" '[ ! "$(docker ps -a | grep "web_search_dataset")" ] && docker run --name web_search_dataset cloudsuite/web-search:dataset'
+
+# Copy load generator .jar, if not exists
+JAR_NAME="httploadgenerator.jar"
+if [ ! -f "$PWD/$JAR_NAME" ]; then
+	if [ ! -f "$LOAD_GENERATOR_LOC/$JAR_NAME" ]; then
+		printf "expected load generator jar at %s\n" "$LOAD_GENERATOR_LOC/$JAR_NAME" 1>&2
+		exit 1
+	fi
+	cp "$LOAD_GENERATOR_LOC/$JAR_NAME" "$PWD/$JAR_NAME"
+fi
 
 # Tuple format CPU, Mem, Profile
 #        |++++ #1 +++++| |+++++ #2 +++++|
